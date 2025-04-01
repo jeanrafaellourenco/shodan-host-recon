89f0cbe8004046abf65eddcb5c8a75bcec79b747
jeanrafaellourenco <162214206+jeanrafaellourenco@users.noreply.github.com>
2025-04-01 16:45:38 -0300

    Update shodan_recon.py

--- shodan_recon.py
+++ shodan_recon.py
@@ -19,7 +19,7 @@ if not IP or not re.match(r"^(?:\d{1,3}\.){3}\d{1,3}$", IP):
     exit()
 
 URL = f"https://www.shodan.io/host/{IP}"
-HEADERS = {"User-Agent": "Mozilla/5.0 (ReconBot)"}
+HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"}
 
 # === Requisição ===
 response = requests.get(URL, headers=HEADERS)
